8b3fcab28541ae0760371e9e2b152381e8b26019
Shane <shane@smiskol.com>
2020-05-13 22:21:40 -0500

    fix for steer rate > 100

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -80,6 +80,10 @@ class CarController():
       apply_steer_req = 0
     else:
       apply_steer_req = 1
+      if abs(CS.out.steeringRate) > 100:
+        apply_steer = 0
+        apply_steer_req = 0
+        self.steer_rate_limited = True
 
     if not enabled and CS.pcm_acc_status:
       # send pcm acc cancel cmd if drive is disabled but pcm is still on, or if the system can't be activated
